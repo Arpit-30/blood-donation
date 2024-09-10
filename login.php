51812ead1912ea030226bc68f3b577409b84c23e
Arpit-30 <104922278+Arpit-30@users.noreply.github.com>
2024-09-10 17:59:12 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="nav.css">
+</head>
+<body>
+    <form action="" method="post">
+   <?php  
+   include("header.php");
+   ?>
+    </form>
+</body>
+</html>
